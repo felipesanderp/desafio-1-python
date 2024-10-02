ade228feb63284f4fc32be836ba75c725a5ee8cb
felipesanderp <felipe.panisson@hotmail.com>
2024-10-02 11:36:04 -0300

    function to add contact and to see contacts

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
+def adicionar_contato(contatos, nome_contato, telefone, email):
+    contato = {"nome": nome_contato, "telefone": telefone, "email": email, "favorito": False}
+    contatos.append(contato)
+    print("\nContato {nome_contato} adiconado com sucesso!")
+
+def ver_contatos(contatos):
+    print("\n Lista de contatos:")
+    for indice, contato in enumerate(contatos, start=1):
+        favorito = "★" if contato["favorito"] else " "
+        nome_contato = contato["nome"]
+        print(f"{indice}. [{favorito}] {nome_contato}")
 
 
+contatos = []
 
 while True:
     print("\n Menu do gerenciador de Lista de contatos:")
@@ -14,7 +26,14 @@ while True:
     escolha = input("Digite sua escolha: ")
 
     if escolha == "1":
-        print("Escolha 1")
+        nome_contato = input("Digite o nome do contato que deseja adicionar:")
+        telefone = input("Digite o telefone do contato:")
+        email = input("Agora o e-mail:")
+
+        adicionar_contato(contatos, nome_contato, telefone, email)
+    
+    elif escolha == "2":
+        ver_contatos(contatos)
     
     elif escolha == "7":
         break
