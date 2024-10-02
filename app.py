d59ecef2a0a88b3422b1013e8bc04021a899b65e
felipesanderp <felipe.panisson@hotmail.com>
2024-10-02 11:25:14 -0300

    menu options with while

---
+++ app.py
@@ -0,0 +1,22 @@
+
+
+
+while True:
+    print("\n Menu do gerenciador de Lista de contatos:")
+    print("1. Adiconar contato")
+    print("2. Ver contatos")
+    print("3. Atualizar contato")
+    print("4. Marcar contato como favorito")
+    print("5. Ver contatos favoritos")
+    print("6. Deletar contato")
+    print("7. Sair")
+
+    escolha = input("Digite sua escolha: ")
+
+    if escolha == "1":
+        print("Escolha 1")
+    
+    elif escolha == "7":
+        break
+
+print("Programa finalizado!")
